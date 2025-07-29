ee1feb42c1252b65be7c3aab6dbb4c79c9f584d8
msalterhslu <mark.salter@hslu.ch>
2025-07-29 16:28:26 +0200

    Check ilias user returned by evento mapping still exists

--- classes/import/action/UserImportActionDecider.php
+++ classes/import/action/UserImportActionDecider.php
@@ -6,6 +6,7 @@ use EventoImportLite\import\data_management\ilias_core_service\IliasUserServices
 use EventoImportLite\import\action\user\UserActionFactory;
 use EventoImportLite\communication\api_models\EventoUser;
 use EventoImportLite\import\data_management\repository\IliasEventoUserRepository;
+use ilObjUser;
 
 class UserImportActionDecider
 {
@@ -37,6 +38,13 @@ class UserImportActionDecider
     public function determineImportAction(EventoUser $evento_user) : EventoImportLiteAction
     {
         $matched_user_id = $this->evento_user_repo->getIliasUserIdByEventoId($evento_user->getEventoId());
+
+        if ($matched_user_id !== null) {
+            if (!ilObjUser::userExists([$matched_user_id])) {
+                $this->evento_user_repo->deleteEventoIliasUserConnectionByEventoId($evento_user->getEventoId());
+                $matched_user_id = null;
+            }
+        }
 
         if ($matched_user_id === null) {
             return $this->matchToIliasUsersAndDetermineAction($evento_user);
